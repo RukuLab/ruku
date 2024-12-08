8c9be83448ae7ffa0a71808aa189c75c912d9fa4
Rafi <mr.k779@outlook.com>
2024-12-08 02:32:16 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -122,7 +122,6 @@ async fn main() {
         }
         Command::GitReceivePack { repo } => {
             log.section("... RUKU ...");
-            let _ = get_ruku_config(&log, repo, &server_config);
             git.cmd_git_receive_pack(repo);
         }
         Command::GitUploadPack { repo } => {
